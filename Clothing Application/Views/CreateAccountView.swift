04b33ce2bbcaf3e5f25c4af7b4640f9913deeda9
Dulashi <ashinidulashi67@gmail.com>
2024-03-19 14:03:54 +0530

    Created and implemented the CreateAccountView in Views Category.

--- Clothing Application/Views/CreateAccountView.swift
+++ Clothing Application/Views/CreateAccountView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct CreateAccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("My Account")
+                .font(.title)
+                .padding(.top, 50)
+            
+            Text("GlamCloth")
+                .font(.title)
+                .padding()
+            
+            TextField("First Name", text: $firstName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Last Name", text: $lastName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            SecureField("Confirm Password", text: $confirmPassword)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button(action: {
+                AccountView()
+            }) {
+                Text("Create Account")
+            }
+            .padding()
+            
+            NavigationLink(destination: LoginView()) {
+                Text("Already have an account? Sign In")
+            }
+            .padding()
+        }
+        .padding()
     }
 }
+
 
 #Preview {
     CreateAccountView()
 }
+
